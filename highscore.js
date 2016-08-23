da62eed9a4edda2543ba3d5225c02245ab44804a
Simon Wallner <me@simonwallner.at>
2016-08-23 16:26:01 +0200

    now with tags and only one data sample at a time

--- highscore.js
+++ highscore.js
@@ -6,43 +6,53 @@ var svg;
 
 var margin = {top: 10, right: 30, bottom: 30, left: 30};
 var width = 500 - margin.left - margin.right;
-var height = 200 - margin.top - margin.bottom;
+var height = 100 - margin.top - margin.bottom;
+var data = {};
+var tags = [];
+
 
 
 window.onload = function() {
 
-	svg = d3.select("body").append("svg")
-		.attr("width", width + margin.left + margin.right)
-		.attr("height", height + margin.top + margin.bottom)
-		 .append("g")
-			.attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 }
 
 
 libsw.onMessage = function(dataIn) {
 	if (dataIn.type == 'com.lostinthegarden.highscore') {
 
+		var payload = dataIn.payload;
+		if (!data[payload.tag])
+		{
+			data[payload.tag] = [];
+			tags.push(payload.tag);
+		}
+
+		data[payload.tag].push(payload.time);
+
 		var margin = window.margin;
 		var width = window.width;
 		var height = window.height;
 
-		// quick fix: start clean
-		d3.select('svg').remove();
-		svg = d3.select("body").append("svg")
-			.attr("width", width + margin.left + margin.right)
-			.attr("height", height + margin.top + margin.bottom)
-			 .append("g")
-				.attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+		var body = d3.select('body');
+		var div = body.selectAll('div.plot').data(tags);
+		div.enter().append('div')
+			.attr('id', function(d, i) { return d; })
+			.attr('class', 'plot')
+			.append('p')
+				.text(function(d, i) { return d; });
+		div.exit()
+			.remove();
 
-		// Generate a Bates distribution of 10 random variables.
-		var values = dataIn.payload.bestTimes.map(function(element) {
-			return element / 1000.0; // convert to seconds
-		});
 
-		var currentLap = dataIn.payload.time;
+		var minTime = tags.reduce(function(previous, current) {
+			return Math.min(previous, d3.min(data[current]));
+		}, 0)
+		var maxTime = tags.reduce(function(previous, current) {
+			return Math.max(previous, d3.max(data[current]));
+		}, 0)
 
 		var x = d3.scale.linear()
-			.domain([d3.min(values), d3.max(values)])
+			.domain([minTime -0.1, maxTime + 0.1])
 			.range([0, width]);
 
 		var xAxis = d3.svg.axis()
@@ -52,49 +62,67 @@ libsw.onMessage = function(dataIn) {
 		// A formatter for counts.
 		var formatCount = d3.format(",.0f");
 
-		// Generate a histogram using twenty uniformly-spaced bins.
-		var data = d3.layout.histogram()
-			.bins(x.ticks(20))
-			(values);
 
-		var y = d3.scale.linear()
-			.domain([0, d3.max(data, function(d) { return d.y; })])
-			.range([height, 0]);
+		for (var i = 0; i < tags.length; i++)
+		{
+			var tag = tags[i];
+			values = data[tag];
+
+			// quick fix: start clean
+			d3.select('#' + tag + ' svg').remove();
+			var svg = d3.select('#' + tag).append("svg")
+				.attr("width", width + margin.left + margin.right)
+				.attr("height", height + margin.top + margin.bottom)
+				 .append("g")
+					.attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
 
-		var bar = svg.selectAll(".bar")
-		    .data(data)
-		  .enter().append("g")
-		    .attr("class", "bar")
-		    .attr("transform", function(d) { return "translate(" + x(d.x) + "," + y(d.y) + ")"; });
+			// Generate a histogram using twenty uniformly-spaced bins.
+			var histData = d3.layout.histogram()
+				.bins(x.ticks(20))
+				(values);
 
-		bar.append("rect")
-		    .attr("x", 1)
-		    .attr("width", (x(data[1].x) - x(data[0].x)) - 1)
-		    .attr("height", function(d) { return height - y(d.y); });
+			var y = d3.scale.linear()
+				.domain([0, d3.max(histData, function(d) { return d.y; })])
+				.range([height, 0]);
 
-		bar.append("text")
-		    .attr("dy", ".75em")
-		    .attr("y", 6)
-		    .attr("x", (x(data[1].x) - x(data[0].x)) / 2)
-		    .attr("text-anchor", "middle")
-		    .text(function(d) { return formatCount(d.y); });
 
-		svg.append("g")
-		    .attr("class", "x axis")
-		    .attr("transform", "translate(0," + height + ")")
-		    .call(xAxis);
 
-		// find current bin
-		var i;
-		for (i = 0; i < data.length; i++) {
-			if (currentLap < (data[i].x + data[i].dx)) {
-				break;
-			}
+			var bar = svg.selectAll(".bar")
+			    .data(histData)
+			  		.enter().append("g")
+			    		.attr("class", "bar")
+			    		.attr("transform", function(d) { return "translate(" + x(d.x) + "," + y(d.y) + ")"; });
+
+
+			bar.append("rect")
+			    .attr("x", 1)
+			    .attr("width", (x(histData[1].x) - x(histData[0].x)) - 1)
+			    .attr("height", function(d) { return height - y(d.y); });
+
+			bar.append("text")
+			    .attr("dy", ".75em")
+			    .attr("y", 6)
+			    .attr("x", (x(histData[1].x) - x(histData[0].x)) / 2)
+			    .attr("text-anchor", "middle")
+			    .text(function(d) { return formatCount(d.y); });
+
+			svg.append("g")
+			    .attr("class", "x axis")
+			    .attr("transform", "translate(0," + height + ")")
+			    .call(xAxis);
+
+			// // find current bin
+			// var i;
+			// for (i = 0; i < histData.length; i++) {
+			// 	if (currentLap < (histData[i].x + histData[i].dx)) {
+			// 		break;
+			// 	}
+			// }
+			// i = Math.min(i, histData.length - 1);
+			//
+			// var bar = d3.selectAll('rect')[0][i]
+			// d3.select(bar).attr('class', 'currentLap');
 		}
-		i = Math.min(i, data.length - 1);
-
-		var bar = d3.selectAll('rect')[0][i]
-		d3.select(bar).attr('class', 'currentLap');
 	}
 }
